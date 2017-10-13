72284d6658a8636414e5b93471652499505880e3
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-10-13 16:45:22 +0200

    Removes getInitialProps

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
-    return { html, head, errorHtml, chunks }
-  }
-
   render () {
     return (
       <html lang='en'>

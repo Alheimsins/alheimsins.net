52113cb972c374c5b169dd92da9bde1773c6df41
maccyber <jonas.enge@gmail.com>
2019-04-13 22:00:34 +0200

    easter

---
+++ pages/easter.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <img src="/static/fibonacci_fibrot_circle.png" style={{ height: '100%', width: '100%', objectFit: 'contain' }} />
+  </Layout>
+)
